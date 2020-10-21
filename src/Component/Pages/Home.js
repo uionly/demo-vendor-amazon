e0cdca17b47afe4517b288f45069d679ed87afa3
tahreen zeba <tahreenzeba1@gmail.com>
2020-10-20 20:41:40 -0400

    Pending intro and details completed

--- src/Component/Pages/Home.js
+++ src/Component/Pages/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Wrapper from '../Layout/Container';
 import styled from 'styled-components';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, ListGroupItem } from 'react-bootstrap';
 
 const ContentText = styled.div`
   text-align: center;
@@ -11,12 +11,10 @@ const ContentText = styled.div`
   }
   .colLeft {
     padding: 2%;
-    text-align: left;
     border-right: 1px solid orange;
   }
   .colRight {
     padding: 2%;
-    text-align: right;
   }
   .Header{
     font-weight: bold;
@@ -32,6 +30,18 @@ const ContentText = styled.div`
     font-size: 18px;
     font-weight: bold;
   }
+  .paragraph{
+    font-size:14px;
+    font-weight: bold;
+    line-height: 20px;
+
+  }
+
+  .listItem{
+    border: none;
+    marin-top: 1%;
+
+  }
 `;
 
 class Home extends Component {
@@ -44,18 +54,41 @@ class Home extends Component {
             <Col className='colLeft' md={6}>
               <h6 className='Heading'>Learning</h6>
               <hr />
-              <p>
-                The app is for the training process of the Associate inside the
-                Amazon building only.
+              <p className='paragraph'>
+                <strong>This app should be access inside Amazon LGA-9 building ONLY.</strong>
               </p>
+              <p className='paragraph'>
+                The reason behind creating this application is to make the process more
+                simplified to understand for both the ambassador plus the 
+                associates who will be trainned in the new process.
+              </p>
+              <div className='paragraph'>
+                With the help of this <strong>"Demo Training"</strong>, we can
+
+                <br/>
+                  <ListGroupItem className="listItem">Quickly access all proccess.</ListGroupItem>
+                  <ListGroupItem className="listItem">Never miss any step in the processing.</ListGroupItem>
+                  <ListGroupItem className="listItem">Easy to read and understand.</ListGroupItem>
+                  <ListGroupItem className="listItem"></ListGroupItem>
+
+              </div>
             </Col>
             <Col className='colRight' md={6}>
               <h6 className='Heading'>About us</h6>
               <hr />
-              <p>
-                The app is for the training process of the Associate inside the
-                Amazon building only.
-              </p>
+              <p className='paragraph'>
+              I am Zeba, Ambassador of Amazon LGA-9, my motto behind creating this application 
+                is to make these words alive <br/>
+                <strong>"Work Hard,  Have Fun  and  Make History"</strong>.
+              </p><p className='paragraph'>
+              I have been with Amazon for more than 2 years now, Amazon is changing rapidly for its customers.
+                It always comes up with new strategies, technologies to help their customers.
+                Hence this is my attempt to go back home everyday without <strong>"Stress"</strong> but with
+                 <strong> "Good memories"</strong> and time spend with my co-workers. 
+                 </p><p className='paragraph'>
+                   You will be able to see my work cum study progress update on this site eventually.
+                   </p>
+            
             </Col>
           </Row>
         </Container>
